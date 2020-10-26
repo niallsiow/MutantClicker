46503108d785202f0ffe32f96672bb1710408deb
niallsiow@yahoo.co.uk <niallsiow@yahoo.co.uk>
2020-10-26 22:20:53 +0000

    buttonScript but better

---
+++ MutantClicker/Assets/ButtonScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(() => ButtonClicked(1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ButtonClicked(int buttonNo)
+    {
+        Debug.Log("button is clicked" );
+    }
+}
