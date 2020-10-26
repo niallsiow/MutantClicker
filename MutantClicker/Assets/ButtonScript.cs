c84d497aa6170f5857edb655a22049f886990616
ChrisW-EcoSurv <57662885+ChrisW-EcoSurv@users.noreply.github.com>
2020-10-26 22:51:18 +0000

    Update ButtonScript.cs

--- MutantClicker/Assets/ButtonScript.cs
+++ MutantClicker/Assets/ButtonScript.cs
@@ -13,6 +13,8 @@ public class ButtonScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        textBox.text = counter.ToString();
+
         button.onClick.AddListener(() => ButtonClicked(1));
     }
 
