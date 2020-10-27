61c57fe9051e65d02ca3e4ffbadd9d4e62192662
niallsiow@yahoo.co.uk <niallsiow@yahoo.co.uk>
2020-10-27 13:08:52 +0000

    added an auto-clicker

--- MutantClicker/Assets/ButtonScript.cs
+++ MutantClicker/Assets/ButtonScript.cs
@@ -5,28 +5,52 @@ using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
-    public Button button;
+    public Button clickButton;
+    public Button autoClickerButton;
+
+
     public Text textBox;
 
     private int counter = 0;
+    private int autoClickers = 0;
+
+
+    float timeBetweenAutoClicks = 1f;
+    float timeSinceAutoClicks = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         textBox.text = "Sacrifices: " + counter.ToString();
 
-        button.onClick.AddListener(() => ButtonClicked(1));
+        clickButton.onClick.AddListener(() => ButtonClicked(1));
+
+        autoClickerButton.onClick.AddListener(() => AutoClickerButtonClicked());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        textBox.text = "Sacrifices: " + counter.ToString();
+
+        if(Time.time > timeSinceAutoClicks)
+        {
+            timeSinceAutoClicks = Time.time + timeBetweenAutoClicks;
+            counter = counter + (1 * autoClickers);
+        }
     }
 
     void ButtonClicked(int buttonNo)
     {
         counter++;
-        textBox.text = "Sacrifices: " + counter.ToString();
+    }
+
+    void AutoClickerButtonClicked()
+    {
+        if(counter > 10)
+        {
+            autoClickers++;
+            counter = counter - 10;
+        }
     }
 }
